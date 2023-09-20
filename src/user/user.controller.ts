8f3026f20fffee76b7569f32dcd5ab5c26cc4020
ZHOU sebastien <sebastien.4084@gmail.com>
2023-09-20 14:56:05 +0200

    commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,24 +5,18 @@ export class UserController {
     constructor(private userService: UserService) {}
 
     checkUsername(username: string): boolean {
-        if (username === '' || username.trim() === '') return false;
+        if (username === '') return false;
         return true;
     }
 
     add(username: string): User {
         if (!this.checkUsername(username)) throw new Error('Username is invalid');
-        // is the username empty ?
-        // is the username whitespaced ?
-        // other checks...
         return this.userService.add(username);
     }
 
     getById(id: number): User | null {
         if (id < 0) throw new Error('Id cannot be negative');
         if (id % 1 !== 0) throw new Error('Id cannot be decimal');
-        // is the id a decimal ?
-        // is the id a negative number ?
-        // other checks...
         return this.userService.getById(id);
     }
 }
--- src/user/user.json-service.ts
+++ src/user/user.json-service.ts
@@ -1,12 +1,30 @@
 import { User } from './user';
 import { UserService } from './user.service';
+import * as fs from 'fs';
 
 export class UserJSONService implements UserService {
     add(username: string): User {
-        throw new Error('Method not implemented.');
+        let maxId = 0;
+        const user = JSON.parse(fs.readFileSync('./src/user/users.json', 'utf8')).users;
+        for(let i = 0; i < user.length; i++){
+            if (user[i].id > maxId) {
+                maxId = user[i].id;
+            }
+        }
+        const newUser = new User(maxId + 1, username);
+        user.push(newUser);
+        fs.writeFileSync('./src/user/users.json', "{\"users\":"+JSON.stringify(user)+"}", 'utf8');
+        return user;
     }
 
     getById(id: number): User | null {
-        throw new Error('Method not implemented.');
+        const user = JSON.parse(fs.readFileSync('./src/user/users.json', 'utf8')).users;
+        console.log(user[0].id);
+        for(let i = 0; i < user.length; i++){
+            if (user[i].id === id) {
+                return user[i];
+            }
+        }
+        return null;
     }
 }
